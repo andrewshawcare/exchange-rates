0c4f9fd6cba17611cb8da97c4f0917824004fda8
Andrew Shaw Care <andrewshawcare@gmail.com>
2019-07-17 08:59:47 -0400

    Adding dist to repository for deployment with GitHub Pages

---
+++ dist/bundle.js
@@ -0,0 +1,7 @@
+var Button = ({ label = "" } = {}) => {
+  const buttonElement = document.createElement("button");
+  buttonElement.innerText = label;
+  return buttonElement;
+};
+
+document.body.appendChild(Button({ label: "Hello, world!" }));
